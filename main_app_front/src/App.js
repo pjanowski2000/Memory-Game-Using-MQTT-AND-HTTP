d9e278c1edb7b0045526586660abda0d4a5cfc42
Pawel Janowski <mpjanowski@wp.pl>
2021-01-01 23:26:04 +0100

    simple publish

--- main_app_front/src/App.js
+++ main_app_front/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React, {useState,useEffect} from 'react';
 import './App.css';
-
+let mqtt    = require('mqtt');
+let options = { clientId: 'service-3Kx03pKnM2',
+connectTimeout: 5000,
+hostname: '10.45.3.14',
+port: 8000,
+path: '/mqtt',
+name: 'chat'
+};
+let client  = mqtt.connect(options);
+client.subscribe('test');
 function App() {
+  
+  const [Mesg, setMesg] = useState('czekam na coś')
+  useEffect(() => {
+    client.on('message', function (test, message) {
+     let wiadomosc = message.toString();
+      // Updates React state with message 
+      setMesg(wiadomosc);
+      console.log(wiadomosc);
+      
+      });
+    
+  }, [])
+  
+  
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+    <h1>Początek Projektu Czatu </h1>
+        {Mesg}
+        </header>
+        </div>
   );
 }
 
