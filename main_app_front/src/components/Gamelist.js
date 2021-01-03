ffd7943bd3db9a8e80480bb0bef1be8df8bdac9a
Pawel Janowski <mpjanowski@wp.pl>
2021-01-03 14:13:54 +0100

    Game List improvement

--- main_app_front/src/components/Gamelist.js
+++ main_app_front/src/components/Gamelist.js
@@ -1,32 +1,75 @@
-import React, { useState } from 'react';
-const Gamelist =({user})=>{
-    
-    if (Selector === '') {
-        return (
-            <div className="App">
-                <div onClick={() => game()}>Gra</div>
-                <div    onClick={() => chat()} >Chat</div>
-            </div>
-        );
+import React, { useState, useEffect } from 'react';
+import uuidv4 from 'uuid';
+
+const axios = require('axios');
+
+const Gamelist = ({ user }) => {
+
+    const [gamelist, setgamelist] = useState([])
+    const [Newgame, setNewgame] = useState('')
+    useEffect(() => {
+        axios.get('http://localhost:3050/')
+            .then(function (response) {
+                setgamelist(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }, [])
+    function refresh() {
+        axios.get('http://localhost:3050/')
+            .then(function (response) {
+                setgamelist(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
     }
-    if (Selector === 'chat') {
-        return (
-            <div className="App">
-                <header className="App-header">
-                <Chatstarter></Chatstarter>
-                </header>
-            </div>
-        )
+    const ChangeHandler=(e)=>{setNewgame(e.target.value)}
+    const sendNewGame=(event)=>{
+        axios.post('http://localhost:3050/', {
+           id:Newgame
+          })
+          .then(function (response) {
+            if(response.data){
+                axios.get('http://localhost:3050/')
+            .then(function (response) {
+                setgamelist(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            }
+            else{
+                alert(`Gra o nazwie ${Newgame} już istnieje`)
+            }
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      setNewgame('')
+      event.preventDefault();
+
+
     }
-    if (Selector === 'game') {
-        return (
-            <div className="App">
-                <header className="App-header">
-                <Gamestarter> </Gamestarter>
-                </header>
-            </div>
-        )
-    }
+
+    let view = gamelist.map((elem) => (<div key={uuidv4()} >  {elem} </div>))
+    return (
+        <div className="App">
+            <h1>Create New Game</h1>
+            <h3><form onSubmit={sendNewGame} >
+                <label className="ChatForm">
+                    New Game Name:
+        <input type="text" value={Newgame} onChange={ChangeHandler} />
+                </label>
+                <input type="submit" value="Send" />
+            </form> </h3>
+            <h1 onClick={() => refresh()}>Game List</h1>
+            {view}
+
+        </div>
+    )
 }
 
-export default Selector;
+
+export default Gamelist;
